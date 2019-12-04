f9f8a47755324baec863b102339a893e25422636
Praveen Bonakurthi <prbona@microsoft.com>
2019-12-05 00:27:35 +0530

    DP coin changes added.

--- PlayGround/CoinChangeProblem.cs
+++ PlayGround/CoinChangeProblem.cs
@@ -11,13 +11,17 @@ namespace PlayGround
             Console.WriteLine("Coin change problem.");
 
             int total = 6;
-            int[] coins = { 1,2,3,4 };
+            int[] coins = { 1, 2, 3, 4 };
             //Dictionary<int, int> map = new Dictionary<int, int>();
             //int topDownValue = MinCoinTopDown(total, coins, map);
             //int bottomUpValue = MinimumCoinBottomUp(total, coins);
             int count = 0;
             CoinChangeSolutions(new List<int>(), total, coins, 0, ref count);
             Console.WriteLine(count);
+            CN(coins, total, 0, new List<int>());
+            Console.WriteLine("DP");//same as knapsack problem
+            var s = CoinChangeWaysDP(6, coins);
+            Console.WriteLine(s);
             //Console.WriteLine(String.Format("Bottom up and top down result {0} {1}", bottomUpValue, topDownValue));
 
             Console.ReadLine();
@@ -128,6 +132,78 @@ namespace PlayGround
                 start = start - coins[j];
             }
             Console.WriteLine();
+        }
+
+        static void CN(int[] coins, int total, int current, List<int> result)
+        {
+            if (total == 0)
+            {
+                Console.WriteLine(string.Join("-", result));
+                return;
+            }
+
+            for (int i = current; i < coins.Length; i++)
+            {
+                if (total >= coins[i])
+                {
+                    result.Add(coins[i]);
+                    CN(coins, total - coins[i], i, result);
+                    result.Remove(coins[i]);
+                }
+            }
+        }
+
+        static int CoinChangeWaysDP(int amount, int[] coins)
+        {
+
+            /*
+              Each row represents using the denominations up to that point in
+              the denominations array (see the video explanation)
+            */
+            int[,] dp = new int[coins.Length + 1, amount + 1];
+
+            /*
+              Max ways to make change for 0 will be 1, doing nothing.
+            */
+            dp[0,0] = 1;
+
+            for (int i = 1; i <= coins.Length; i++)
+            {
+                /*
+                  Set the subproblem for the amount of 0 to 1 when
+                  solving this row
+                */
+                dp[i,0] = 1;
+
+                for (int j = 1; j <= amount; j++)
+                {
+                    int currentCoinValue = coins[i - 1];
+
+                    /*
+                      dp[i,j] will be the sum of the ways to make change not considering
+                      this coin (dp[i-1,j]) and the ways to make change considering this
+                      coin (dp[i,j] - currentCoinValue] ONLY if currentCoinValue <= j, otherwise
+                      this coin can not contribute to the total # of ways to make change at this
+                      sub problem target amount)
+                    */
+                    int withoutThisCoin = dp[i - 1,j];
+                    int withThisCoin = 0;
+                    if (currentCoinValue <= j)
+                    {
+                        withThisCoin = dp[i, j - currentCoinValue];
+                    }
+                    //int withThisCoin = currentCoinValue <= j ? dp[i,j - currentCoinValue] : 0;
+
+                    dp[i,j] = withoutThisCoin + withThisCoin;
+                }
+            }
+
+            /*
+              The answer considering ALL coins for the FULL amount is what
+              we want to return as the answer
+            */
+            dp.Dump();
+            return dp[coins.Length,amount];
         }
     }
 }
