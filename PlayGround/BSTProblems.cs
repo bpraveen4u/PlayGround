7c13f37642d28d56c2cd53037cf1ab3f147f5ece
Praveen Bonakurthi <prbona@microsoft.com>
2019-12-21 00:11:06 +0530

    BST problems

---
+++ PlayGround/BSTProblems.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PlayGround
+{
+    class BSTProblems
+    {
+        public static void Main()
+        {
+            Console.WriteLine("BST Problems");
+
+            var root = GetBST();
+            InOrder(root);
+            Swap(root, root.Left.Right);
+            Console.WriteLine();
+            InOrder(root);
+            Console.WriteLine();
+
+            FixBST(root);
+            InOrder(root);
+            Console.ReadLine();
+
+        }
+
+        static void Swap(Node<int> a, Node<int> b)
+        {
+            var temp = a.Data;
+            a.Data = b.Data;
+            b.Data = temp;
+        }
+
+        static void FixBST(Node<int> root)
+        {
+            Node<int> current = null;
+            Node<int> middle = null;
+            Node<int> last = null;
+            Node<int> prev = null;
+            //InOrder(root, ref prev, ref current, ref middle, ref last);
+            InOrder(root, ref prev, ref current, ref last);
+
+            //swap only data of current and last
+            if (current != null && last != null)
+            {
+                Swap(current, last);
+            }
+            //else
+            //{
+            //    Swap(current, middle);
+            //}
+        }
+
+        static Node<int> GetBST()
+        {
+            Node<int> root = new Node<int>(15);
+            root.Left = new Node<int>(10);
+            root.Left.Left = new Node<int>(8);
+            root.Left.Right = new Node<int>(12);
+            root.Right = new Node<int>(20);
+            root.Right.Left = new Node<int>(16);
+            root.Right.Right = new Node<int>(25);
+
+            return root;
+        }
+
+        static void InOrder(Node<int> root)
+        {
+            if (root != null)
+            {
+                InOrder(root.Left);
+                Console.Write(" " + root.Data);
+                InOrder(root.Right);
+            }
+        }
+
+        static void InOrder(Node<int> root, ref Node<int> prev, ref Node<int> current, ref Node<int> middle, ref Node<int> last)
+        {
+            //extra variable used
+            if (root != null)
+            {
+                InOrder(root.Left, ref prev, ref current, ref middle, ref last);
+                // 2 5 7 6
+                if (prev != null && prev.Data > root.Data)
+                {
+                    if (current == null)
+                    {
+                        current = prev;
+                        middle = root;
+                    }
+                    else
+                        last = root;
+                }
+
+                prev = root;
+
+                InOrder(root.Right, ref prev, ref current, ref middle, ref last);
+            }
+        }
+
+        static void InOrder(Node<int> root, ref Node<int> prev, ref Node<int> current, ref Node<int> last)
+        {
+            //simpler and handles all the cases, dont forget to use ref variable or class level variables
+            if (root != null)
+            {
+                InOrder(root.Left, ref prev, ref current, ref last);
+                // 2 5 7 6
+                if (prev != null && prev.Data > root.Data)
+                {
+                    if (current == null)
+                    {
+                        current = prev;
+                    }
+                    
+                    last = root;
+                }
+
+                prev = root;
+
+                InOrder(root.Right, ref prev, ref current, ref last);
+            }
+        }
+    }
+}
--- PlayGround/GenerateAllStringBits.cs
+++ PlayGround/GenerateAllStringBits.cs
@@ -9,8 +9,12 @@ namespace PlayGround
         public static void Main()
         {
             Console.WriteLine("N Bits string for given n value");
-            NBits(3, new int[3]);
-
+            NBits(4, new int[4]);
+            var arr = new int[] { 0, 1, 1, 1 };
+            Reverse(arr);
+            DecToBinary(17);
+            Console.WriteLine();
+            BecToBinaryBits(5);
             Console.ReadLine();
         }
 
@@ -18,7 +22,9 @@ namespace PlayGround
         {
             if (n == 0)
             {
-                Console.WriteLine(string.Join(",", result));
+                int[] clone = (int[])result.Clone();
+                Reverse(clone);
+                Console.WriteLine(string.Join("", clone));
             }
             else
             {
@@ -26,6 +32,166 @@ namespace PlayGround
                 NBits(n - 1, result);
                 result[n - 1] = 1;
                 NBits(n - 1, result);
+            }
+        }
+
+        static void Reverse(int[] items)
+        {
+            int i = 0;
+            int j = items.Length - 1;
+            while (i < j)
+            {
+                var temp = items[i];
+                items[i] = items[j];
+                items[j] = temp;
+                i++;
+                j--;
+            }
+
+            return;
+        }
+
+        static void DecToBinary(int n)
+        {
+            // array to store binary number 
+            int[] binaryNum = new int[32];
+
+            // counter for binary array 
+            int i = 0;
+            while (n > 0)
+            {
+                // storing remainder in 
+                // binary array 
+                binaryNum[i] = n % 2;
+                n = n / 2;
+                i++;
+            }
+
+            // printing binary array 
+            // in reverse order 
+            for (int j = i - 1; j >= 0; j--)
+                Console.Write(binaryNum[j]);
+        }
+
+        static void BecToBinaryBits(int n)
+        {
+            // Size of an integer is assumed to be 32 bits 
+            for (int i = 31; i >= 0; i--)
+            {
+                int k = n >> i;
+                if ((k & 1) > 0)
+                    Console.Write("1");
+                else
+                    Console.Write("0");
+            }
+        }
+
+        public static char[] Base64Encoding(byte[] data)
+        {
+            int length, length2;
+            int blockCount;
+            int paddingCount;
+
+            length = data.Length;
+
+            if ((length % 3) == 0)
+            {
+                paddingCount = 0;
+                blockCount = length / 3;
+            }
+            else
+            {
+                paddingCount = 3 - (length % 3);
+                blockCount = (length + paddingCount) / 3;
+            }
+
+            length2 = length + paddingCount;
+
+            byte[] source2;
+            source2 = new byte[length2];
+
+            for (int x = 0; x < length2; x++)
+            {
+                if (x < length)
+                {
+                    source2[x] = data[x];
+                }
+                else
+                {
+                    source2[x] = 0;
+                }
+            }
+
+            byte b1, b2, b3;
+            byte temp, temp1, temp2, temp3, temp4;
+            byte[] buffer = new byte[blockCount * 4];
+            char[] result = new char[blockCount * 4];
+
+            for (int x = 0; x < blockCount; x++)
+            {
+                b1 = source2[x * 3];
+                b2 = source2[x * 3 + 1];
+                b3 = source2[x * 3 + 2];
+
+                temp1 = (byte)((b1 & 252) >> 2);
+
+                temp = (byte)((b1 & 3) << 4);
+                temp2 = (byte)((b2 & 240) >> 4);
+                temp2 += temp;
+
+                temp = (byte)((b2 & 15) << 2);
+                temp3 = (byte)((b3 & 192) >> 6);
+                temp3 += temp;
+
+                temp4 = (byte)(b3 & 63);
+
+                buffer[x * 4] = temp1;
+                buffer[x * 4 + 1] = temp2;
+                buffer[x * 4 + 2] = temp3;
+                buffer[x * 4 + 3] = temp4;
+
+            }
+
+            for (int x = 0; x < blockCount * 4; x++)
+            {
+                result[x] = SixBitToChar(buffer[x]);
+            }
+
+            switch (paddingCount)
+            {
+                case 0:
+                    break;
+                case 1:
+                    result[blockCount * 4 - 1] = '=';
+                    break;
+                case 2:
+                    result[blockCount * 4 - 1] = '=';
+                    result[blockCount * 4 - 2] = '=';
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+
+        private static char SixBitToChar(byte b)
+        {
+            char[] lookupTable = new char[64] {
+        'A','B','C','D','E','F','G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z',
+        '0','1','2','3','4','5','6','7','8','9','+','/'
+                };
+
+            if ((b >= 0) && (b <= 63))
+            {
+                return lookupTable[(int)b];
+            }
+            else
+            {
+                return ' ';
             }
         }
     }
